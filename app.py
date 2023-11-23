7fb59384b99b90e68ef94caeca95fc4d8a6fbc71
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-23 11:13:39 -0300

    show_editar_paciente

--- app.py
+++ app.py
@@ -29,25 +29,49 @@ def show_pacientes(pacientes):
     st.subheader("Lista de Pacientes")
 
     # Mostra o nome dos pacientes como uma lista
-    selected_patient = st.selectbox("Selecione um paciente: ", list(pacientes.keys()))
+    selected_patient = st.selectbox("Selecione um paciente: ", [paciente["Nome"] for paciente in pacientes])
 
     # Mostra a ficha do paciente
     if selected_patient:
-        paciente = pacientes[selected_patient]
-        st.write("Nome: ", paciente["Nome"])
-        st.write("Idade: ", paciente["Idade"])
-        st.write("Endereço: ", paciente["Endereço"])
-        st.write("Convênio: ", paciente["Convênio"])
-        st.write("Doenças Preexistentes:", paciente["Doenças Preexistentes"])
-
-
+        paciente = next((p for p in pacientes if p["Nome"] == selected_patient), None)
+        if paciente:
+            st.write("Nome: ", paciente["Nome"])
+            st.write("Idade: ", paciente["Idade"])
+            st.write("Endereço: ", paciente["Endereço"])
+            st.write("Convênio: ", paciente["Convenio"])
+            st.write("Doenças Preexistentes:", paciente["Doenças Preexistentes"])
 
 
 
 
 def show_editar_paciente(pacientes):
-    return
+    st.subheader("Editar Informações do Paciente")
 
+    # Mostra o nome dos pacientes como uma lista
+    selected_patient = st.selectbox("Selecione um paciente: ", [paciente["Nome"] for paciente in pacientes])
+
+    # Mostra o formulário para editar as informações do paciente selecionado
+    if selected_patient:
+        paciente = next((p for p in pacientes if p["Nome"] == selected_patient), None)
+        if paciente:
+            st.write("Nome: ", paciente["Nome"])
+            idade = st.number_input("Idade: ", value=paciente["Idade"])
+            endereco = st.text_input("Endereço: ", value=paciente["Endereço"])
+            convenio = st.text_input("Convênio: ", value=paciente["Convenio"])
+            doencas_preexistentes = st.text_input("Doenças Preexistentes: ", value=", ".join(paciente["Doenças Preexistentes"]))
+
+            # Atualiza as informações do paciente no dicionário
+            paciente["Idade"] = idade
+            paciente["Endereço"] = endereco
+            paciente["Convenio"] = convenio
+            paciente["Doenças Preexistentes"] = [d.strip() for d in doencas_preexistentes.split(",")]
+
+            # Salva as alterações no arquivo pacientes.json
+            with open("pacientes.json", "w", encoding="utf-8") as file:
+                json.dump(pacientes, file, indent=4)
 
 def show_marcar_consulta(pacientes):
     return
+
+if __name__ == "__main__":
+    main()
