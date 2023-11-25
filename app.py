32a10863b8b91d319a0f283862dcf0aa970866be
rafaelnxd <rafaelnovaes46@outlook.com>
2023-11-25 19:26:52 -0300

    Salvar_exames, salvar_prescricao, salvar_notas

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import json
+import os
 
 def carregar_pacientes():
     with open("pacientes.json", "r", encoding="utf-8") as file:
@@ -44,7 +45,7 @@ def show_pacientes(pacientes):
             st.write("Endereço: ", paciente["Endereço"])
             st.write("Convênio: ", paciente["Convenio"])
             st.write("Doenças Preexistentes:", paciente["Doenças Preexistentes"])
-            st.write("Consulta: ", paciente.get("Consulta", "Sem consulta marcada"))
+            st.write("Próxima Consulta: ", paciente.get("Próxima Consulta", "Sem consulta marcada"))
 
 def show_editar_paciente(pacientes):
     st.subheader("Editar Informações do Paciente")
@@ -87,7 +88,7 @@ def show_marcar_consulta(pacientes):
             horario_consulta = st.time_input("Horário da Consulta: ")
 
             # Salva as informações da consulta no dicionário do paciente
-            paciente["Consulta"] = {
+            paciente["Próxima Consulta"] = {
                 "Data": str(data_consulta),
                 "Horário": str(horario_consulta)
             }
@@ -105,32 +106,35 @@ def show_notas_paciente(pacientes):
 
     # Mostra a aba de notas para o paciente selecionado
     if selected_patient:
-        paciente = next((p for p in pacientes if p["Nome"] == selected_patient), None )
+        paciente = next((p for p in pacientes if p["Nome"] == selected_patient), None)
         if paciente:
             st.write("Nome: ", paciente["Nome"])
             st.write("Idade: ", paciente["Idade"])
             st.write("Consulta: ", paciente.get("Consulta", "Sem consulta marcada"))
 
-
             # Adiciona as notas para o paciente
             st.subheader("Recomendações Médicas: ")
-            nova_nota = st.text_area("Digite o seu comentário: ")
-            adicionar_nota = st.button("Adicionar Nota")
+            nova_nota = st.text_area("Digite o seu comentário:", key=f"nota_{selected_patient}")
+            adicionar_nota = st.button("Adicionar Nota", key=f"btn_nota_{selected_patient}")
 
             if adicionar_nota and nova_nota:
                 paciente["Notas"] = paciente.get("Notas", []) + [nova_nota]
+                salvar_notas(nova_nota, selected_patient)  # Salva a nota na pasta "notas"
                 st.success("Nota adicionada com sucesso!")
 
             # Adiciona upload de prescrição
             st.subheader("Fazer Upload de Prescrição: ")
-            prescricao_upload = st.file_uploader("Selecione o arquivo:")
-
+            prescricao_upload = st.file_uploader("Selecione o arquivo:", key=f"prescricao_{selected_patient}")
             if prescricao_upload:
-                # Salva o arquivo no diretório uploads
-                path = path.join("prescricoes",  selected_patient + "_prescricao.pdf")
-                with open(path,  "wb") as f:
-                    f.write(prescricao_upload.read())
+                salvar_prescricao(prescricao_upload, selected_patient)
                 st.success("Prescrição enviada com sucesso!")
+
+            # Adiciona upload de exames
+            st.subheader("Fazer Upload de Exames: ")
+            exames_upload = st.file_uploader("Selecione o arquivo:", key=f"exames_{selected_patient}")
+            if exames_upload:
+                salvar_exames(exames_upload, selected_patient)
+                st.success("Exames enviados com sucesso!")
 
             # Mostra as notas do paciente
             notas = paciente.get("Notas", [])
@@ -138,6 +142,35 @@ def show_notas_paciente(pacientes):
                 st.subheader("Notas: " )
                 for nota in notas:
                     st.write(nota)
+
+def salvar_exames(upload, paciente_nome):
+    # Cria o diretório "exames" se não existir
+    if not os.path.exists("exames"):
+        os.makedirs("exames")
+
+    # Define o caminho do arquivo
+    path = os.path.join("exames", f"{paciente_nome}_exame.pdf")
+
+    # Salva o exame no arquivo
+    with open(path, "wb") as f:
+        f.write(upload.read())
+        
+def salvar_prescricao(upload, paciente_nome):
+    path = os.path.join("prescricoes", f"{paciente_nome}_prescricao.pdf")
+    with open(path, "wb") as f:
+        f.write(upload.read())
+
+def salvar_notas(nova_nota, paciente_nome):
+    # Cria o diretório "notas" se não existir
+    if not os.path.exists("notas"):
+        os.makedirs("notas")
+
+    # Define o caminho do arquivo
+    path = os.path.join("notas", f"{paciente_nome}_nota.txt")
+
+    # Salva a nota no arquivo
+    with open(path, "w", encoding="utf-8") as file:
+        file.write(nova_nota)
 
 if __name__ == "__main__":
     main()
